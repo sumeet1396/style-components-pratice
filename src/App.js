3e399ca87023c4b389176534d0865e261f29cf8c
rsumeetprdxn <sumeetr@prdxn.com>
2021-05-22 17:52:15 +0530

    form component added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
+import React, { useState } from 'react';
+import CourseInput from './Components/CourseGoals/CourseGoalInput/CourseInput';
 import './App.css';
 
 function App() {
+
+  const [courseGoals, setCourseGoals] = useState([
+    { text: 'Do all exercises!', id: 'g1' },
+    { text: 'Finish the course!', id: 'g2' }
+  ]);
+
+  const addGoalHandler = enteredText => {
+    setCourseGoals(prevGoals => {
+      const updatedGoals = [...prevGoals];
+      updatedGoals.unshift({ text: enteredText, id: Math.random().toString() });
+      return updatedGoals;
+    });
+  };
+
   return (
     <div className="App">
-  
+      <div id="goal-form">
+        <CourseInput onAddGoal={addGoalHandler} />
+      </div>
     </div>
   );
 }
